6cbc5313a367904c9cb10a7759746143c18c085d
Hector Churata <hec000ale@gmail.com>
2020-12-10 00:27:46 -0400

    Fin de la sección 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { APP_ROUTES } from './app.routes';
 
 // Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
+
+// servicios
+import { ServiceModule } from './services/service.module';
 
 // componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
-import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -24,7 +29,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SiderbarService } from '../../services/service.index';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _siderbar: SiderbarService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
